79cf140f33c62c044ad546988a4ba97587c0b8ca
顾彬 <1107593449@qq.com>
2016-12-09 17:35:34 +0800

    集成了第三方登录分享++++

--- YohoNow/app/src/main/java/com/eueh/yohonow/video/videoviedeo/videovideoson/VideoVideoSonActivity.java
+++ YohoNow/app/src/main/java/com/eueh/yohonow/video/videoviedeo/videovideoson/VideoVideoSonActivity.java
@@ -26,12 +26,15 @@ import com.google.gson.Gson;
 
 import java.util.HashMap;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class VideoVideoSonActivity extends BaseActivity implements View.OnClickListener {
     private TextView tvTop, tvLeft, tvRight;
     private VideoView videoView;
     private WebView webView;
     private WebSettings webSettings;
-    private ImageView ivFinishMe;
+    private ImageView ivFinishMe,ivShare;
 
     @Override
     public int setLayou() {
@@ -45,7 +48,10 @@ public class VideoVideoSonActivity extends BaseActivity implements View.OnClickL
         tvLeft = (TextView) findViewById(R.id.tv_left_me);
         tvRight = (TextView) findViewById(R.id.tv_right_me);
         ivFinishMe = (ImageView) findViewById(R.id.iv_finish_me);
+        ivShare = (ImageView) findViewById(R.id.iv_share);
+
         ivFinishMe.setOnClickListener(this);
+        ivShare.setOnClickListener(this);
 
         webView = (WebView) findViewById(R.id.tv_vebview);
 
@@ -59,6 +65,9 @@ public class VideoVideoSonActivity extends BaseActivity implements View.OnClickL
 
     @Override
     public void initData() {
+
+        //分享
+        ShareSDK.initSDK(this);
 
         Intent intent = getIntent();
         String idVideoSon = intent.getStringExtra("idVideoSon");
@@ -118,7 +127,39 @@ public class VideoVideoSonActivity extends BaseActivity implements View.OnClickL
             case R.id.iv_finish_me:
                 finish();
                 break;
+            case R.id.iv_share:
+                showShare();
+                break;
         }
+    }
+
+
+    private void showShare() {
+            ShareSDK.initSDK(this);
+            OnekeyShare oks = new OnekeyShare();
+//关闭sso授权
+            oks.disableSSOWhenAuthorize();
+
+// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+            oks.setTitle("标题");
+// titleUrl是标题的网络链接，QQ和QQ空间等使用
+            oks.setTitleUrl("http://sharesdk.cn");
+// text是分享文本，所有平台都需要这个字段
+            oks.setText("我是分享文本");
+// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+// url仅在微信（包括好友和朋友圈）中使用
+            oks.setUrl("http://sharesdk.cn");
+// comment是我对这条分享的评论，仅在人人网和QQ空间使用
+            oks.setComment("我是测试评论文本");
+// site是分享此内容的网站名称，仅在QQ空间使用
+            oks.setSite(getString(R.string.app_name));
+// siteUrl是分享此内容的网站地址，仅在QQ空间使用
+            oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+            oks.show(this);
+
     }
 }
 
